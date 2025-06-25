675a5d21c2ddb208f8da7e194714cb50ea5f2061
Shin Yaw Yang <syty03@gmail.com>
2025-06-26 05:05:56 +0800

    first commit

---
+++ Day1/day1.py
@@ -0,0 +1,11 @@
+first_name = "bro"
+
+favorite_food = "Calamari"
+
+print(f"Hello {first_name}")
+
+print(f"Your favorite food {first_name} is {favorite_food}")
+
+age = 22
+
+print(f"You are {age} years old")
