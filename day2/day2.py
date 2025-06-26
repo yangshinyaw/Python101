581c35f0c598cbf10af0c25de0a294f9af461383
Shin Yaw Yang <syty03@gmail.com>
2025-06-27 04:39:26 +0800

    day 2 of python101

---
+++ day2/day2.py
@@ -0,0 +1,16 @@
+#String
+full_name = "Shin Yang"
+
+#int
+age = 22
+
+#float
+weight = 75.4
+
+#boolean
+is_legalAge = True
+
+if is_legalAge:
+    print(f"Congrats {full_name}, since you are {age} years old, and have a weight of {weight}kg, you can pass!")
+else:
+    print("Not compatible")
