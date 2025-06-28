821ba59926ce1a55b1197e9e9c6d9f189c1f9dc3
Shin Yaw Yang <syty03@gmail.com>
2025-06-28 22:52:11 +0800

    day 4 of python101

---
+++ day4.py/day4.py
@@ -0,0 +1,10 @@
+#Madlibs game
+adjective1 = input("Enter an adjective: ")
+adjective2 = input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+
+print(f"Today I went to the Mall and I looked for {noun1}")
+print(f"Little did I know that this store was not {adjective2} at all")
+print(f"I came up with this speculation because people said that this was {adjective1}")
+print(f"After some time I heard someone {verb}")
