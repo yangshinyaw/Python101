dada80b2ee423a1cc1cb5be32b55703530a31f56
Shin Yaw Yang <syty03@gmail.com>
2025-06-30 13:10:02 +0800

    day6 of python101

--- day6/day6.py
+++ day6/day6.py
@@ -1,5 +1,6 @@
 #Python calculator
 
+'''
 operator = input("Enter an operator (+ - * /): ")
 
 num1 = int(input("Enter first number: "))
@@ -15,3 +16,22 @@ elif operator == "/":
     print(num1 /num2)
 else:
     print(f"{operator} is not valid")
+'''
+
+#Weight Conversion
+
+weight = float(input("Enter your weight: "))
+unit = input("Kilograms or Pounds? (K or L): ")
+
+if unit == "K":
+    weight *= 2.205
+    unit = "Lbs."
+    print(f"Your weight is: {round(weight, 2)} {unit}")
+elif unit == "L":
+    weight /= 2.205
+    unit = "Kgs."
+    print(f"Your weight is: {round(weight, 2)} {unit}")
+else:
+    print(f"{unit} is not valid")
+
+
