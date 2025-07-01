bc3197edf0d346cef562f9b6ba052e124f67b429
Shin Yaw Yang <syty03@gmail.com>
2025-07-01 13:09:01 +0800

    day6 logical operators of python101

--- day6/day6-logical-operators.py
+++ day6/day6-logical-operators.py
@@ -1,5 +1,5 @@
 # or
-
+'''
 age = 17
 is_holiday = False
 
@@ -7,3 +7,26 @@ if age > 18 or is_holiday:
     print("You have classes today")
 else:
     print("You don't have classes today")
+    
+# or
+
+temperature = 35
+is_raining = True
+
+if temperature > 30 or is_raining:
+    print("Stay indoors today")
+else:
+    print("You can go outside")
+'''
+'''
+# and
+
+temp = 18
+is_sunny = True
+
+if temp > 20 and is_sunny:
+    print("Don't go outside, it's HOT today")
+else:
+    print("The weather today is great! You can go outside")
+
+'''
