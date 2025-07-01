5b9960b1d39e7fb56446024b5814bdabbd6955d9
Shin Yaw Yang <syty03@gmail.com>
2025-07-01 13:21:40 +0800

    day7 conditional expression of python101

---
+++ day7/day7-conditional-expression.py
@@ -0,0 +1,30 @@
+# conditionl expressions
+
+# Sample 1
+age = 14
+
+result = "Legal age" if age > 18 else "Minor"
+
+print(result)
+
+
+# Sample 2
+temp = 33
+
+weather = "Hot" if temp > 30 else "Cold"
+
+print(weather)
+
+# Sample 3
+num = 8
+
+odd_even = "EVEN" if num % 2 == 1 else "ODD"
+
+print(odd_even)
+
+# Sample 4
+user_role = "member"
+
+access = "Access granted" if user_role == "admin" else "Access denied"
+
+print(access)
