a5c3b6c734306f1ab19de1d16852e7073734cd85
Shin Yaw Yang <syty03@gmail.com>
2025-07-02 17:09:03 +0800

    day8 string indexing of python101

---
+++ day8/day8-string-indexing.py
@@ -0,0 +1,12 @@
+#string indexing
+
+# [start : end : step]
+
+phone_number = "0961-487-3084"
+
+#print(phone_number[0:13:5])        
+#print(phone_number[0:])           
+#print(phone_number[-5])     
+#print(phone_number[::3])
+#print(phone_number[-4:])
+#print(phone_number[::-1])
