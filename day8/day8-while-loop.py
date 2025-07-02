7ae601c75ea323f3336131b6bcbe463aac827f4c
Shin Yaw Yang <syty03@gmail.com>
2025-07-03 04:34:00 +0800

    day8 while loo[ of python101

--- day8/day8-while-loop.py
+++ day8/day8-while-loop.py
@@ -1,7 +1,28 @@
 #while loop
-ame = input("Enter your name: ")
+'''
+name = input("Enter your name: ")
 
 while name == "":
     print("Please enter your name!")
     name = input("Enter your name: ")
 print(f"Hello {name}, Welcome to the System!")
+'''
+
+'''
+age = int(input("How old are you: "))
+
+while age < 0:
+    print("Not valid age!")
+    age = int(input("How old are you: "))
+print(f"You are {age} years old")
+'''
+
+food = input("Enter your favorite food (Q to quit): ")
+
+while not food == "Q":
+    print(f"You like {food}")
+    food = input("Enter your favorite food (Q to quit): ")
+print("Good Bye, See you again!")
+
+
+
