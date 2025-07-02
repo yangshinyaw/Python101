54182a386232786138f94984db1bde5175949383
Shin Yaw Yang <syty03@gmail.com>
2025-07-03 04:26:08 +0800

    day8 while loop of python101

---
+++ day8/day8-while-loop.py
@@ -0,0 +1,7 @@
+#while loop
+ame = input("Enter your name: ")
+
+while name == "":
+    print("Please enter your name!")
+    name = input("Enter your name: ")
+print(f"Hello {name}, Welcome to the System!")
