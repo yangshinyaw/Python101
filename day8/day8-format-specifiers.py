ae6367ccd5f82aba4cd715b00929530f8ca8476f
Shin Yaw Yang <syty03@gmail.com>
2025-07-03 04:15:56 +0800

    day8 format specifiers of python101

---
+++ day8/day8-format-specifiers.py
@@ -0,0 +1,13 @@
+# format specifiers
+
+price1 = 62319.872
+price2 = 32113.83223
+price3 = -214211.7622241
+
+#print(f"Price is: {price1:.2f}")
+#print(f"Price is: {price2:.3f}")
+#print(f"Price is: {price3:.4f}")
+
+print(f"Price is: {price1:+,.2f}")
+print(f"Price is: {price2:+,.2f}")
+print(f"Price is: {price3:+,.2f}")
