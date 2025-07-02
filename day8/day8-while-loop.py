a1dda739ce0a6a63e5d57ce04efcdb82861b720d
Shin Yaw Yang <syty03@gmail.com>
2025-07-03 04:37:26 +0800

    day8 while loop of python101

--- day8/day8-while-loop.py
+++ day8/day8-while-loop.py
@@ -17,12 +17,22 @@ while age < 0:
 print(f"You are {age} years old")
 '''
 
+'''
 food = input("Enter your favorite food (Q to quit): ")
 
 while not food == "Q":
     print(f"You like {food}")
     food = input("Enter your favorite food (Q to quit): ")
 print("Good Bye, See you again!")
+'''
 
+
+num = int(input("Enter number (1-10): "))
+
+while num <= 1 or num > 10:
+    print(f"{num} is out of range")
+    num = int(input("Enter number (1-10): "))
+
+print(f"Congrats, you picked number {num}")
 
 
