3f4c1b857206b96ccd263ed539769251970553a4
Shin Yaw Yang <syty03@gmail.com>
2025-07-07 00:06:55 +0800

    day10 for loop of python 101

---
+++ day10/day10-for-loop.py
@@ -0,0 +1,18 @@
+#for x in range(2, 9):
+#   print(x)
+
+#for x in reversed(range(1,10)):
+#    print(x)
+
+'''
+phone_number = "0961-487-3084"
+
+for x in phone_number:
+    print(x)
+'''
+
+for x in range(1,10):
+    if x == 3:
+        continue
+    else:
+        print(x)
